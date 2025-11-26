febd62aeae9e6862d37c2484f7cf41a9bc5bef54
Iulian Masar <ext-iulian.masar@mangopay.com>
2025-11-26 21:48:03 +0200

    version bump 3.47.2

--- MangoPay/Libraries/RestTool.php
+++ MangoPay/Libraries/RestTool.php
@@ -12,7 +12,7 @@ use Psr\Log\LoggerInterface;
  */
 class RestTool
 {
-    const VERSION = '3.47.1';
+    const VERSION = '3.47.2';
 
     /**
      * Root/parent instance that holds the OAuthToken and Configuration instance
